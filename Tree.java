47d81a073303e13c8efaff70cdeb721a91138f11
Vishal <mailtovishal29@gmail.com>
2023-07-17 21:54:10 +0530

    first project

---
+++ Tree.java
@@ -0,0 +1,16 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args){
+        // comment
+        System.out.println("Hey there, I’m Vishal and I’m learning to code in Java!");
+        System.out.println("Ready to get my hands dirty!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+
+    }
+
+}
